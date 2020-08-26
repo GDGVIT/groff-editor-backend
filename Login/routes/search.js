eb958c4aa0996c88302782a9ca134fb6bc5758ad
V1NT4G3C0D3 <pragatibhattad1610@gmail.com>
2020-08-26 14:09:01 +0530

    add express-validator

--- Login/routes/search.js
+++ Login/routes/search.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 const mongoose = require("mongoose");
 const Search = require("../models/search.js");

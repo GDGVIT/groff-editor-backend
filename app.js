421b4813b5b52f0fd4d73e9e93c414ebc56ad7e0
V1NT4G3C0D3 <pragatibhattad1610@gmail.com>
2020-09-06 14:22:30 +0530

    :wrench: switch to socket

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const socket = require("socket.io");
-const WebSocket = require("ws");
+const socket = require("socket.io");
+// const WebSocket = require("ws");
 
 const jwt = require("jsonwebtoken");
 const { exec } = require("child_process");
@@ -59,64 +59,12 @@ app.use("/preview", previewRoute);
 
 // web socket[handle incoming req]
 
-const wss = new WebSocket.Server({server});
-let child;
-wss.on('connection', (person)=>{
-  console.log(`made socket connection : ${person.id}`);
-
-  person.on("cmd", function cmd(val) {
-    let val_json = JSON.parse(val);
-
-    let token = val_json.token;
-    let user_id = val_json.user_id;
-    let fileName = val_json.fileName;
-    let data = val_json.data;
-    let email;
-    try {
-      email = jwt.verify(token, process.env.JWT_KEY);
-    } catch (err) {
-      console.log(err);
-    }
-    
-    User.updateOne(
-      {
-        _id: user_id,
-        "files.fileName": fileName,
-      },
-      {
-        $set: { "files.$.fileData": data },
-      }
-    )
-      .exec()
-      .then((result) => {
-        console.log("File updated");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    let command = 'printf "' + data + '"';
-
-    child = exec(`${command} | groff -i -ms -T html`, (err, stdout, stderr) => {
-      if (err) {
-        console.log(`Error: ${err.message}`);
-      }
-      if (stderr) {
-        console.log(`Error: ${stderr}`);
-      }
-      // console.log(stdout)
-      person.send("cmd", stdout);
-    });
-  });
-});
-
+// const wss = new WebSocket.Server({server});
 // let child;
-// var io = socket(server);
-// io.origins('*:*');
-// io.on("connection", (person) => {
+// wss.on('connection', (person)=>{
 //   console.log(`made socket connection : ${person.id}`);
 
-//   person.on("cmd", function (val) {
+//   person.on("cmd", function cmd(val) {
 //     let val_json = JSON.parse(val);
 
 //     let token = val_json.token;
@@ -157,7 +105,59 @@ wss.on('connection', (person)=>{
 //         console.log(`Error: ${stderr}`);
 //       }
 //       // console.log(stdout)
-//       person.emit("cmd", stdout);
+//       person.send("cmd", stdout);
 //     });
 //   });
 // });
+
+let child;
+var io = socket(server);
+io.origins('*:*');
+io.on("connection", (person) => {
+  console.log(`made socket connection : ${person.id}`);
+
+  person.on("cmd", function (val) {
+    let val_json = JSON.parse(val);
+
+    let token = val_json.token;
+    let user_id = val_json.user_id;
+    let fileName = val_json.fileName;
+    let data = val_json.data;
+    let email;
+    try {
+      email = jwt.verify(token, process.env.JWT_KEY);
+    } catch (err) {
+      console.log(err);
+    }
+    
+    User.updateOne(
+      {
+        _id: user_id,
+        "files.fileName": fileName,
+      },
+      {
+        $set: { "files.$.fileData": data },
+      }
+    )
+      .exec()
+      .then((result) => {
+        console.log("File updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    let command = 'printf "' + data + '"';
+
+    child = exec(`${command} | groff -i -ms -T html`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(`Error: ${err.message}`);
+      }
+      if (stderr) {
+        console.log(`Error: ${stderr}`);
+      }
+      // console.log(stdout)
+      person.emit("cmd", stdout);
+    });
+  });
+});

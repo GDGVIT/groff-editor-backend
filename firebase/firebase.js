26d7cd75bf51a213db7dfac2f5f936efea450915
Mayank Kumar <mayank22oct@gmail.com>
2021-04-10 20:53:02 +0530

    fix: replaced firebase.json as an environment variable

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin')
-const serviceAccount = require('../firebase.json')
+const serviceAccount = process.env.FIREBASE
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)

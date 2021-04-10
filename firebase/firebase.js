580e477d0bd265cf8fb6d012f7b780941920bbf3
Mayank Kumar <mayank22oct@gmail.com>
2021-04-10 21:00:28 +0530

    fix: new safe.sh

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin')
 const serviceAccount = process.env.FIREBASE
 
 admin.initializeApp({
-    credential: admin.credential.cert(JSON.stringify(serviceAccount))
+    credential: admin.credential.cert(serviceAccount)
 })
 
 module.exports = admin
--- safe.sh
+++ safe.sh
@@ -1,3 +1,5 @@
-export FIREBASE="$(cat firebase.json)"
-rm firebase.json
+DIR="$RANDOM$RANDOM$RANDOM"
+export FIREBASE="$DIR/firebase.json"
+mkdir $DIR
+mv firebase.json $DIR
 npm start
